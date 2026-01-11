6b30cc67d7cce9b50cb537ac7e119682d24987c3
Hisuwi <hisuwi.dev@gmail.com>
2026-01-11 11:22:49 +0800

    ain

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module goth
 go 1.25.5
 
 require github.com/a-h/templ v0.3.977
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,39 @@ package main
 
 import "net/http"
 
+type ContactForm struct {
+	Initial          bool
+	SubmitButtonText string
+
+	Name  string
+	Email string
+	Error string
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		Index().Render(r.Context(), w)
+		View(ContactForm{}).Render(r.Context(), w)
 	})
 	http.ListenAndServe(":8080", nil)
+}
+
+func (m *ContactForm) InputValidation() {
+	if m.Initial {
+		return
+	}
+
+	if m.Name == "" {
+		if m.Error != "" {
+			m.Error += "\n"
+		}
+		m.Error += "*Name is required"
+	}
+
+	if m.Email == "" {
+		if m.Error != "" {
+			m.Error += "\n"
+		}
+		m.Error += "*Email is required"
+	}
 }
